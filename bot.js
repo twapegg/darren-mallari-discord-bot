fae76e63faabcfc74b48767e8ba674c86d4fa268
Ian Alt <John Stanley Altonaga>
2023-06-03 17:23:43 +0800

    Add other features

--- bot.js
+++ bot.js
@@ -1,23 +1,65 @@
-require("dotenv").config();
+const { Client, IntentsBitField, MessageAttachment } = require("discord.js");
 
-const { Client, Intents } = require('discord.js');
-
-const intents = new Intents([
-  Intents.FLAGS.GUILDS,
-  Intents.FLAGS.GUILD_MESSAGES,
-  // Add other necessary intents here
-]);
-
-const client = new Client({ intents });
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
 
 // Rest of your bot code
 
-client.login('YOUR_BOT_TOKEN');
 client.on("ready", () => {
   console.log("Our bot is ready to go!!");
 });
 
-client.on("message", (msg) => {
-  if (msg.content === "Ping") msg.reply("Pong!");
+client.on("messageCreate", (msg) => {
+  if (msg.content === "ping") msg.reply("Pong!");
 });
 
+client.on("messageCreate", (msg) => {
+  if (msg.content === "yo darren") msg.reply("ye?");
+});
+
+client.on("messageCreate", (msg) => {
+  if (msg.mentions.has(client.user)) msg.reply("Wassup Nigga!");
+});
+
+client.on("messageCreate", (msg) => {
+  if (msg.content === "get em darren")
+    msg.reply("nigger nigger nigger nigger nigger");
+});
+
+client.on("messageCreate", (msg) => {
+  // if message equals a certain value, reply with an image
+  if (msg.content === "vroom vroom") {
+    msg.reply({
+      files: [
+        {
+          attachment: "./images/crash.jpg",
+          name: "crash.jpg",
+        },
+      ],
+    });
+  }
+});
+
+client.on("messageCreate", (msg) => {
+  // if message contains goated
+  if (msg.content.includes("goat")) {
+    msg.reply({
+      files: [
+        {
+          attachment: "./images/goat.jpg",
+          name: "goat.jpg",
+        },
+      ],
+    });
+  }
+});
+
+client.login(
+  "ODEwODk0NTI4MDIxNTI4NjY2.Gp2McE.1eYRzaxuZ5BEmp-xL686sNjfbVQOx2RZKhMKBE"
+);
